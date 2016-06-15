cdd2ced7c4c3e7825e3fb2e9177fedd4ee5e074c
piecyk <piecyk@gmail.com>
2016-06-15 18:05:20 +0100

    init commit

---
+++ src/future_app/db.cljs
@@ -0,0 +1,8 @@
+(ns future-app.db
+  (:require [schema.core :as s :include-macros true]))
+
+;; schema of app-db
+(def schema {:greeting s/Str})
+
+;; initial state of app-db
+(def app-db {:greeting "Test 123"})
